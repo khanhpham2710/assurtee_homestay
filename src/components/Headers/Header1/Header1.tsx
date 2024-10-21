b26e2544e72121c254aad56ba03a135150983816
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-10-21 11:37:41 +0700

    remove layout add header

--- src/components/Headers/Header1/Header1.tsx
+++ src/components/Headers/Header1/Header1.tsx
@@ -1,4 +1,4 @@
-import images from '../../assets/images/images';
+import images from '../../../assets/images/images';
 import { Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
