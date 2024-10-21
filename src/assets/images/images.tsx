5f4dc498e6cd1cb5113f73109e4edf68bcfad94e
Nguyendev037 <nguyenndev037@gmail.com>
2024-10-21 17:34:47 +0700

    fix image

--- src/assets/images/images.tsx
+++ src/assets/images/images.tsx
@@ -3,6 +3,7 @@ import InfoIncon from './frame-48_2024-10-21/frame-48.png';
 import Tooltip from './frame-57_2024-10-21/frame-57.png';
 import Loading from './loading.gif';
 import Success from './invalid-name_2024-10-21/invalid-name.png';
+import InsuranceLogo from '/Image/logo.png'
 import chosedButton from "/Image/group.png";
 import blankButton from "/Image/oval-copy.png";
 import cheroLeft from "/Image/charon_left.png"
@@ -12,6 +13,10 @@ const images = {
     Tooltip,
     Loading,
     Success,
+    chosedButton,
+    blankButton,
+    cheroLeft,
+    InsuranceLogo
 };
 
 export default images;
