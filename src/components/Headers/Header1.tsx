942f155aa6c7d58b1c7712e81b609fbac306b8ba
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-10-22 10:26:16 +0700

    tooltip

--- src/components/Headers/Header1.tsx
+++ src/components/Headers/Header1.tsx
@@ -1,6 +1,7 @@
 import { Box, Tooltip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import images from '../../assets/images';
+import MyToolTip from '../MyTooltip/MyToolTip';
 
 function Header1() {
     const navigate = useNavigate();
@@ -12,7 +13,12 @@ function Header1() {
     return (
         <Box className="header1">
             <img src={images.HomeIcon} onClick={handleClick} />
-            <img src={images.InfoIncon} />
+            <MyToolTip
+                text={[
+                    '본 사이트에서 안내되는 보험료는 보험사의',
+                    '최종 청약 단계의 보험료와 충분히 다를 수 있습니다.',
+                ]}
+            />
         </Box>
     );
 }
