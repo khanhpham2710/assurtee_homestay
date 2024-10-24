53d1265e02f514288603feb6b6a02865552a977b
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-10-24 11:24:37 +0700

    deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './routes/routes';
 import { useLocation } from 'react-router-dom';
 function App() {
