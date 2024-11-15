5a75a89a8c7bdb01107fd7b4c2ac74da5cc159f1
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-11-15 14:53:13 +0700

    remove unneeded resources

--- src/assets/images.tsx
+++ src/assets/images.tsx
@@ -1,107 +1,13 @@
-import HomeIcon from '/Image/Icons/HomeIcon.png';
-import ArrowIconDown from '/Image/Icons/ArrowIconDown.png';
-import ArrowIconRight from '/Image/Icons/ArrowIconRight.png';
-import ImageIcon from '/Image/Icons/ImageIcon.png';
-import Camera from '/Image/Icons/Camera.png';
-import Library from '/Image/Icons/Library.png';
-
-import BackButton from '/Image/Buttons/BackButton.png';
-import chosedButton from '/Image/Buttons/ChosedButton.png';
-import blankButton from '/Image/Buttons/BlankButton.png';
-import videoButton from '/Image/Buttons/videoButton.png';
-import ScrollUp from '/Image/Buttons/ScrollUp.png';
-import ScrollDown from '/Image/Buttons/ScrollDown.png';
-import CloseButton from '/Image/Buttons/CloseButton.png';
-import PNextButton from '/Image/Buttons/pink_NextButton.png';
-
-import blackL from '/Image/Bullets/BlackL.png';
-import greyL from '/Image/Bullets/GreyL.png';
-import whiteL from '/Image/Bullets/WhiteL.png';
-
-import FillOption from '/Image/FillOption.png';
-import ScanOption from '/Image/ScanOption.png';
-import ScanDoc from '/Image/ScanDoc.png';
-
-import AssurLogo from '/Image/Logos/AssureteeLogo.png';
-import mainLogo from '/Image/Logos/ci.png';
-import InsuranceLogo from '/Image/Logos/HLogo.png';
-
-import BackGroundBanner1 from '/Image/MainCarousel/img_PC3x.png';
-import BackGroundBanner2 from '/Image/MainCarousel/img_2PC3x.png';
-import BackGroundBanner3 from '/Image/MainCarousel/img_3PC3x.png';
-
-import item_1 from '/Image/Banner/item_1.png';
-import item_2 from '/Image/Banner/item_2.png';
-import item_3 from '/Image/Banner/item_3.png';
-import item_4 from '/Image/Banner/item_4.png';
-import item_5 from '/Image/Banner/item_5@3x.png';
-
-import KeyDown from '/Image/KeyDown.png';
-
-import CheckBoxBlack from '/Image/CheckBoxes/checkBoxBlack.png';
-import UnCheckBoxGrey from '/Image/CheckBoxes/unCheckBoxGrey.png';
-import CheckBoxWhite from '/Image/CheckBoxes/checkBoxWhite.png';
-import UnCheckBoxWhite from '/Image/CheckBoxes/unCheckBoxWhite.png';
-import PinkCheckBox from '/Image/CheckBoxes/checkBoxPink.png';
-
-import bottom_carousel_1 from '/Image/BottomCarousel/bottom_carousel_1.png';
-import bottom_carousel_2 from '/Image/BottomCarousel/bottom_carousel_2.png';
-import bottom_carousel_3 from '/Image/BottomCarousel/bottom_carousel_3.png';
-
-import Loading from '/Image/Gif/loading.gif';
-import Success from '/Image/success.png';
-
-import FailedRegistration from '/Image/Failed-Registration.png';
-import SuccessRegistration from '/Image/Success-Registration.png';
-
-import FormBackground from '/Image/Form-Background.jpg';
+import videoButton from '/images/videoButton.png';
+import BackGroundBanner1 from '/images/MainCarousel/img_PC3x.png';
+import BackGroundBanner2 from '/images/MainCarousel/img_2PC3x.png';
+import BackGroundBanner3 from '/images/MainCarousel/img_3PC3x.png';
 
 const images = {
-    AssurLogo,
-    InsuranceLogo,
-    mainLogo,
-    HomeIcon,
-    Loading,
-    Success,
-    chosedButton,
-    blankButton,
-    BackButton,
-    blackL,
-    greyL,
-    whiteL,
-    FillOption,
-    ScanOption,
-    ScanDoc,
-    ArrowIconDown,
-    ArrowIconRight,
-    CheckBoxBlack,
-    UnCheckBoxGrey,
-    CheckBoxWhite,
-    UnCheckBoxWhite,
-    PinkCheckBox,
     BackGroundBanner1,
     BackGroundBanner2,
     BackGroundBanner3,
     videoButton,
-    CloseButton,
-    item_1,
-    item_2,
-    item_3,
-    item_4,
-    item_5,
-    KeyDown,
-    bottom_carousel_1,
-    bottom_carousel_2,
-    bottom_carousel_3,
-    ScrollUp,
-    ScrollDown,
-    ImageIcon,
-    Library,
-    Camera,
-    FailedRegistration,
-    SuccessRegistration,
-    PNextButton,
-    FormBackground,
 };
 
 export default images;
